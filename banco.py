a8b413e02f958ac88a1de6ae942f89efdacbe73d
NataliaMacedo <nat.vanessa29@gmail.com>
2024-04-09 23:36:07 -0300

    atualização

--- banco.py
+++ banco.py
@@ -8,3 +8,6 @@ con = lite.connect('dados.db')
 with con:
     cur = con.cursor()
     cur.execute("CREATE TABLE formulario(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, email TEXT, telefone TEXT, dia_em DATE)")
+
+
+
